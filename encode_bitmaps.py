e97b4c29fa14ef6bab74fd16c0f71ceb17c8c6e3
David Ernstrom <david@ernstrom.net>
2007-07-03 02:54:36 +0000

    Encoded the images.py file.

---
+++ encode_bitmaps.py
@@ -0,0 +1,16 @@
+#encode_bitmaps.py
+
+#This file is derived from the wx demo version of the same thing... I just copied it !::)
+
+import sys
+from wx.tools import img2py
+
+command_lines = [
+	"-a -u -n NotebookBKGD bmp_source/BG_image.png images.py",
+	"-a -u -n Splash bmp_source/splash.png images.py"
+	]
+
+if __name__ == "__main__":
+	for line in command_lines:
+		args = line.split()
+		img2py.main(args)
