691531dd608f5b496088cb27c190d01521d24444
David Ernstrom <david@ernstrom.net>
2007-07-15 01:35:39 +0000

    Added get and set methods to my Configuration.py

--- Configuration.py
+++ Configuration.py
@@ -99,41 +99,60 @@ ConfigDefault = {
 class Configuration:
 	def __init__(self, filename):
 		self.filename = filename
+		self.LoadConfigFile()
 
-	def LoadConfigFile(self, config = ConfigDefault):
+	def LoadConfigFile(self):
 		"""
 		returns a dictionary with key's of the form
 		<section>.<option> and the values 
 		"""
-		print config
 		cp = ConfigParser.ConfigParser()
 		cp.read(self.filename)
+		config = {}
 		for sec in cp.sections():
 			name = string.lower(sec)
 			for opt in cp.options(sec):
 				config[name + "." + string.lower(opt)] = string.strip(cp.get(sec, opt))
 		self.CurrentConfig = config
-		return config
+		print "Config:", str(config)
 
-	def write(self, config = None):
+	def SetValueByKey(self, key, value):
+		section, option = key.split('.')
+		self.SetValueByOption(string.lower(section), string.lower(option), value)
+
+	def SetValueByOption(self, section, option, value):
+		self.CurrentConfig[string.lower(section) + '.' + string.lower(option)] = value
+		self.WriteConfigFile()
+
+	def GetValueByKey(self, key):
+		section, option = key.split('.')
+		return self.GetValueByOption(section, option)
+
+	def GetValueByOption(self, section, option):
+		try:
+			value = self.CurrentConfig[section + '.' + option]
+		except KeyError:
+			value = None
+		return value
+
+	def WriteConfigFile(self):
 		"""
 		given a dictionary with key's of the form 'section.option: value'
 		write() generates a list of unique section names
 		creates sections based that list
 		use config.set to add entries to each section
 		"""
-		if not config:
-			config = self.CurrentConfig
 		cp = ConfigParser.ConfigParser()
-		sections = set([k.split('.')[0] for k in config.keys()])
+		sections = set([k.split('.')[0] for k in self.CurrentConfig.keys()])
 		map(cp.add_section, sections)
-		for k,v in config.items():
+		for k,v in self.CurrentConfig.items():
 			s, o = k.split('.')
 			cp.set(s, o, v)
 		cp.write(open(self.filename, "w"))
 
 if __name__=="__main__":
 	ConfigHandle = Configuration("some.cfg")
-	Config = ConfigHandle.LoadConfigFile()
-	print Config
-	ConfigHandle.write(Config)
+	print "Result:", ConfigHandle.GetValueByKey('coolpeople.coolest')
+	ConfigHandle.SetValueByKey('coolpeople.coolest', 'David L Ernstrom')
+	#print ConfigHandle
+	#ConfigHandle.write(Config)
