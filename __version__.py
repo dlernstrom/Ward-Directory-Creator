99f4262cb588d98c9f524ae3d4e0cec1ba41082a
David Ernstrom <david@ernstrom.net>
2014-01-08 01:39:09 +0000

    -Adding the rtree application.

--- __version__.py
+++ __version__.py
@@ -1,4 +1,4 @@
 # Last Time The Person Hit Build:
-#                           Tue Jan 07 10:36:01 2014
+#                           Tue Jan 07 18:38:40 2014
 __version__ = "$Revision$".split(" ")[1]
 
