404764a106143833e67d3798328f3e47e86065e6
David Ernstrom <david@ernstrom.net>
2015-05-20 11:15:20 -0600

    PEP8 refactor

--- Ward Directory Creator.py
+++ Ward Directory Creator.py
@@ -1,20 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 import logging
 import os
 import site
 
 site.addsitedir(os.getcwd())
-#from LoganTools import connections
-#os.environ.setdefault("DJANGO_SETTINGS_MODULE", connections.djangoSettings)
 
 handler = logging.getLogger()
 handler.setLevel(logging.DEBUG)
 handler.addHandler(logging.NullHandler())
-
-#from LoganTools.Logging import FileLoggingManager
-#log = FileLoggingManager.RedirectedFileLogManager(appNameLong = 'Shipping Application', appNameShort = 'Shipping')
-
-#from LoganTools.wxTools import TracebackPlusWX
-#sys.excepthook = TracebackPlusWX.traceback_plus_wx_hook
 
 from application import GUI
 
